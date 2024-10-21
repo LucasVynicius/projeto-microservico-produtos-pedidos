a3e5d09be743232eedfc1a21f931491329ee7b53
Lucas Albuquerque <luucas.albuquerque23@gmail.com>
2024-10-21 15:44:43 -0300

    API pedidos concluido

--- api-pedidos/src/main/java/com/projetopedidos/api_pedidos/ApiPedidosApplication.java
+++ api-pedidos/src/main/java/com/projetopedidos/api_pedidos/ApiPedidosApplication.java
@@ -2,8 +2,10 @@ package com.projetopedidos.api_pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiPedidosApplication {
 
 	public static void main(String[] args) {
